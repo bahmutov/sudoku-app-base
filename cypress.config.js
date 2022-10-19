899a28d971f1b44ef62cf1ddc1fae9e74c039496
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-10-18 22:18:27 -0400

    component config

--- cypress.config.js
+++ cypress.config.js
@@ -7,13 +7,4 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-
-  component: {
-    viewportHeight: 1000,
-    viewportWidth: 1000,
-    devServer: {
-      framework: 'create-react-app',
-      bundler: 'webpack',
-    },
-  },
 })
